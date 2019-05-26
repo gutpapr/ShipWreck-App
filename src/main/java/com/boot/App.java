be911143a4b51225e4f1a485f4687e3c5d0013c0
gutpapr <32028499+gutpapr@users.noreply.github.com>
2019-05-26 19:01:29 +0530

    Update App.java

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * Created by upgundecha on 25/03/16.
  */
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public  class App extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
